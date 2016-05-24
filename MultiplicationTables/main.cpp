2809a49394155b31c9314b54bb9cfb54057fffbe
Anthony Good <anthony@quipper.com>
2016-05-24 19:30:18 +0100

    Initial Commit

---
+++ MultiplicationTables/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MultiplicationTables
+//
+//  Created by Anthony Good on 24/05/2016.
+//  Copyright © 2016 Anthony Good. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
